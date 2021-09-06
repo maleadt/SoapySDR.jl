09ffe014e85adcd16ab5147718cc240ad9870a38
Steve Kelly <kd2cca@gmail.com>
2021-09-06 11:39:50 -0400

    logger squash

--- src/loghandler.jl
+++ src/loghandler.jl
@@ -1,6 +1,13 @@
 #
 # Setup the a handler from SoapySDR log messages into Julia
 
-function logger_soapy2jl(level, message)
-    println("hello from julia")
+function logger_soapy2jl(level, cmessage)
+    message = unsafe_string(cmessage)
+    if level == SOAPY_SDR_FATAL
+        error(message)
+    elseif level == SOAPY_SDR_CRITICAL
+        @warn message
+    else
+        println("SoapySDR_jll: ", message)
+    end
 end
